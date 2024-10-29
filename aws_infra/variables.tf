3b5257877d82e96b33d0094f7032024dd921fe28
Sharuquee <160309748+Sharuquee@users.noreply.github.com>
2024-10-30 04:02:52 +0530

    Create variables.tf

---
+++ aws_infra/variables.tf
@@ -0,0 +1,19 @@
+variable "my_env" {
+  type = string
+}
+
+variable "instance_count" {
+  type = number
+}
+
+variable "ami" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
